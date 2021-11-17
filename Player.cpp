0babbd85cbf1320f2b94e0a0fe2efc315f88a362
MS <s.monidipa@gmail.com>
2021-11-17 22:34:11 +0000

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,7 +3,11 @@ Player::Player(int nRow, int nCol)
 {
 	m_BattleArea = new BattleArea(nRow, nCol);
 }
-
+Player::~Player()
+{
+	if(m_BattleArea)
+		delete m_BattleArea;
+}
 void Player::placeShip(int nHeight, int nWidth, int posX, int posY, eShipType type)
 {
 	m_BattleArea->addShip(type, posX, posY, nHeight, nWidth);
